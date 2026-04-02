4f89e1e981010969f4d0e22a1024d60e9d8f6f6d
JustACat3680 <justacat3680@gmail.com>
2026-04-02 10:07:22 +0300

    edited hello_world.cpp

--- hello_world.cpp
+++ hello_world.cpp
@@ -2,6 +2,8 @@
 using namespace std;
 int main()
 {
-    cout<<"Hello World"<<endl;
+    string name;
+    cout<<"Input name: "; cin>>name;
+    cout<<"Hello World from "<<name<<endl;
     return 0;
 }
