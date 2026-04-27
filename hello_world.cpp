9b9056dcbae9f1e39ac461c1d2331ec5430cfb6e
JustACat3680 <justacat3680@gmail.com>
2026-04-27 08:46:52 +0300

    edited hello_world.cpp

--- hello_world.cpp
+++ hello_world.cpp
@@ -2,8 +2,7 @@
 int main()
 {
     std::string name;
-    std::cout<<"Input name: ";
-    std::cin>>name; //input
-    std::cout<<"Hello World from: "<<name<<std::endl; //output
+    std::cout<<"Input name: "; std::cin>>name; //ввод имени
+    std::cout<<"Hello World from "<<name<<std::endl; //вывод в консоль
     return 0;
 }
